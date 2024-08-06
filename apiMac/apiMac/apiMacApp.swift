44a2dca8689496b68610f012200adf137862876d
dicky0411 <73293265+dicky0411@users.noreply.github.com>
2024-08-06 15:09:04 +0800

    update

---
+++ apiMac/apiMac/apiMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiMacApp.swift
+//  apiMac
+//
+//  Created by Richard Li on 2024-08-06.
+//
+
+import SwiftUI
+
+@main
+struct apiMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
